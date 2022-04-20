d9db19c3f5c93b49123ac8d628d49b001343bd3d
constantgillet <constant.gillet@hetic.net>
2022-04-20 22:26:14 +0200

    Add amazing cursor

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,26 @@
 import Header from "./Header";
 import { Footer } from "./Footer";
+import { useEffect } from "react";
 
 export default function Layout({ children }) {
   return (
     <>
+      <Cursor />
       <Header />
       <main className={"pt-[92px] pb-16"}>{children}</main>
       <Footer />
     </>
   );
+}
+
+function Cursor() {
+  useEffect(() => {
+    const cursor = document.querySelector("#cursor");
+    window.addEventListener("mousemove", (_event) => {
+      cursor.style.top = _event.clientY + "px";
+      cursor.style.left = _event.clientX + "px";
+    });
+  }, []);
+
+  return <div id="cursor"></div>;
 }
